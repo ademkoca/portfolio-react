71a73ee6310795f93ad86933d18f3eee95949564
ademkoca <aademkocaa@gmail.com>
2022-09-12 23:31:57 +0200

    Added the footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer class="text-muted py-5">
+      <div class="container">
+        <p class="float-end mb-1">
+          <a id="backToTop" href="#">
+            Back to top
+          </a>
+        </p>
+        <p class="mb-1">&copy; 2022 Adem Koca</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
