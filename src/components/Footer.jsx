3d32f7b8c46389ddfdec01d50dcd8e66ebfc96ec
ademkoca <aademkocaa@gmail.com>
2022-09-12 23:38:54 +0200

    Added year variable to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <footer class="text-muted py-5">
-      <div class="container">
-        <p class="float-end mb-1">
+    <footer className="text-muted py-5">
+      <div className="container">
+        <p className="float-end mb-1">
           <a id="backToTop" href="#">
             Back to top
           </a>
         </p>
-        <p class="mb-1">&copy; 2022 Adem Koca</p>
+        <p className="mb-1">&copy; {year} Adem Koca</p>
       </div>
     </footer>
   );
