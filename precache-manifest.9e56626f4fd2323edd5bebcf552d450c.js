43db6cc1fe8b2e79c1fd3a1c6fb4bdc06121669c
ademkoca <aademkocaa@gmail.com>
2022-09-12 23:27:38 +0200

    Updates

---
+++ precache-manifest.9e56626f4fd2323edd5bebcf552d450c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ccfc9d6e04af3ffa83c",
+    "url": "/static/css/main.6275f0d9.chunk.css"
+  },
+  {
+    "revision": "0ccfc9d6e04af3ffa83c",
+    "url": "/static/js/main.0ccfc9d6.chunk.js"
+  },
+  {
+    "revision": "bcdfa6a228b128378471",
+    "url": "/static/css/1.fca87df4.chunk.css"
+  },
+  {
+    "revision": "bcdfa6a228b128378471",
+    "url": "/static/js/1.bcdfa6a2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f44e52d07764102594fb72a702db1df",
+    "url": "/index.html"
+  }
+];
